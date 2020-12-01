108f966ef2790c4385b43254399caa8834731eee
Dominic Bartl <dominic.bartl.at@gmail.com>
2020-12-01 11:32:07 +0100

    Add function to get pubsub client

--- src/lib/pub_sup_service.ts
+++ src/lib/pub_sup_service.ts
@@ -5,24 +5,28 @@ import {IPubSubConfig, PubSubConfig, PubSubEventData} from "./types";
 @injectable()
 export class PubSubService {
 
-	private pubsubClient: PubSub;
+    private readonly pubsubClient: PubSub;
 
-	constructor(@inject(PubSubConfig) private config: IPubSubConfig) {
-		this.pubsubClient = new PubSub({
-			projectId: config.projectId,
-		});
-	}
+    constructor(@inject(PubSubConfig) private config: IPubSubConfig) {
+        this.pubsubClient = new PubSub({
+            projectId: config.projectId,
+        });
+    }
 
-	publishMessage<T>(event: PubSubEventData<T>) {
-		return this.publish(event.topic, event.payload);
-	}
+    publishMessage<T>(event: PubSubEventData<T>) {
+        return this.publish(event.topic, event.payload);
+    }
 
-	async publish(topicName: string, data?: any) {
-		const topic = this.getTopic(topicName);
-		return topic.publishJSON(data || {});
-	}
+    async publish(topicName: string, data?: any) {
+        const topic = this.getTopic(topicName);
+        return topic.publishJSON(data || {});
+    }
 
-	getTopic(topicName: string): Topic {
-		return this.pubsubClient.topic(topicName);
-	}
+    getTopic(topicName: string): Topic {
+        return this.pubsubClient.topic(topicName);
+    }
+
+    getClient() {
+        return this.pubsubClient;
+    }
 }
