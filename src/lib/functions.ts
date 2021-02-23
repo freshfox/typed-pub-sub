6c8c599037d1c3892f63bf4cc9d5640fef3534e4
Dominic Bartl <dominic.bartl.at@gmail.com>
2021-02-23 16:32:29 +0100

    Add optional regions parameter to createPubSubFunction

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -1,12 +1,22 @@
 import {PubSubEvent} from "./types";
-import {RuntimeOptions} from "firebase-functions";
+import {RuntimeOptions as BaseOptions} from "firebase-functions";
 import * as functions from "firebase-functions";
+import {SUPPORTED_REGIONS} from "firebase-functions/lib/function-configuration";
+
+export interface RuntimeOptions extends BaseOptions {
+    regions?: typeof SUPPORTED_REGIONS[number][]
+}
 
 type PubSubFunctionCallback<T> = (data: T) => Promise<any> | any;
 
 export function createPubSubFunction<T>(event: PubSubEvent<T>, callback: PubSubFunctionCallback<T>);
-export function createPubSubFunction<T>(event: PubSubEvent<T>, runtimeOptions: RuntimeOptions, callback: PubSubFunctionCallback<T>)
-export function createPubSubFunction<T>(event: PubSubEvent<T>, runtimeOptionsOrCallback: RuntimeOptions | PubSubFunctionCallback<T>, callback?: PubSubFunctionCallback<T>) {
+export function createPubSubFunction<T>(event: PubSubEvent<T>,
+                                        runtimeOptions: RuntimeOptions,
+                                        callback: PubSubFunctionCallback<T>)
+
+export function createPubSubFunction<T>(event: PubSubEvent<T>,
+                                        runtimeOptionsOrCallback: RuntimeOptions | PubSubFunctionCallback<T>,
+                                        callback?: PubSubFunctionCallback<T>) {
     let runtimeOptions: RuntimeOptions = null;
     if (!callback) {
         callback = runtimeOptionsOrCallback as PubSubFunctionCallback<T>;
@@ -14,9 +24,13 @@ export function createPubSubFunction<T>(event: PubSubEvent<T>, runtimeOptionsOrC
         runtimeOptions = runtimeOptionsOrCallback as RuntimeOptions;
     }
 
-    const builder = runtimeOptions
+    let builder = runtimeOptions
         ? functions.runWith(runtimeOptions)
         : functions;
+
+    if (runtimeOptions && runtimeOptions.regions) {
+        builder = builder.region(...runtimeOptions.regions);
+    }
 
     return builder
         .pubsub.topic(event.topic)
