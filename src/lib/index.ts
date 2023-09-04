38aa0ef0812059a9bfd214eac800ef7dfadd1947
Dominic Bartl <dominic.bartl.at@gmail.com>
2023-09-04 15:57:15 +0200

    feat: simplify event creation, remove NestJS dependency

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,37 @@
-export * from './nest_module';
-export * from './pub_sup_service';
-export * from './types';
-export {Message} from '@google-cloud/pubsub'
+import {PubSub} from "@google-cloud/pubsub";
+
+export function publishEvent<T extends PubSubEventPayload>(client: PubSub, event: PubSubEvent<T>) {
+    return client.topic(event.topic).publishMessage({
+        json: event.data || {}
+    });
+}
+
+export type PubSubEventPayload = object | void;
+
+export interface PubSubEventCreator<T extends PubSubEventPayload> {
+    (data: T): PubSubEvent<T>;
+    topic: string;
+    validate?: (data: object) => T
+}
+
+export interface PubSubEvent<T extends PubSubEventPayload> {
+    topic: string;
+    data: T;
+}
+
+export function createPubSubEvent<T extends PubSubEventPayload>(topic: string, validate?: (data: object) => T): PubSubEventCreator<T> {
+    return Object.assign(
+        (data: T) => {
+            return {
+                data, topic
+            };
+        },
+        {
+            topic: topic,
+            validate: validate
+        }
+    );
+}
+
+const AEvent = createPubSubEvent('asd');
+AEvent({});
